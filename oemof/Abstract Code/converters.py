51c9fa5957b56187cd35be6848d519257c57bd3c
agnesfrancois <84960119+agnesfrancois@users.noreply.github.com>
2021-08-20 19:01:05 +0200

    Updated technologies for MOO

--- oemof/Abstract Code/converters.py
+++ oemof/Abstract Code/converters.py
@@ -1,9 +1,9 @@
 import oemof.solph as solph
-from oemof.tools import economics
 import numpy as np
 
 class HeatPumpLinear:
-    def __init__(self, buildingLabel, temperatureDHW, temperatureSH, temperatureLow, input, outputSH, outputDHW, capacityMin, capacityMax, epc, base):
+    def __init__(self, buildingLabel, temperatureDHW, temperatureSH, temperatureLow, input, outputSH, outputDHW, capacityMin, capacityMax,
+                 epc, base, varc, env_flow, env_capa):
         self.__copDHW = self._calculate_cop(temperatureDHW, temperatureLow)
         self.__copSH = self._calculate_cop(temperatureSH, temperatureLow)
         self.__heatpumpSH = solph.Transformer(label='HP_SH'+'__'+buildingLabel, inputs={input: solph.Flow()}, outputs={outputSH: solph.Flow(
@@ -12,8 +12,13 @@ class HeatPumpLinear:
                                             minimum=capacityMin,
                                             maximum=capacityMax,
                                             nonconvex=True,
-                                            offset=base
-                                        ))},
+                                            offset=base,
+                                            env_per_capa=env_capa,
+                                        ),
+                                        variable_costs=varc,
+                                        env_per_flow=env_flow,
+
+                                        )},
                                         conversion_factors={outputSH: self.__copSH})
         self.__heatpumpDHW = solph.Transformer(label='HP_DHW'+'__'+buildingLabel, inputs={input: solph.Flow()}, outputs={outputDHW: solph.Flow(
                                         investment=solph.Investment(
@@ -21,8 +26,13 @@ class HeatPumpLinear:
                                             minimum=capacityMin,
                                             maximum=capacityMax,
                                             nonconvex=True,
-                                            offset=base
-                                        ))},
+                                            offset=base,
+                                            env_per_capa=env_capa,
+                                        ),
+                                        variable_costs=varc,
+                                        env_per_flow=env_flow,
+
+                                        )},
                                         conversion_factors={outputDHW: self.__copDHW})
 
     def _calculate_cop(self, tHigh, tLow):
@@ -48,7 +58,8 @@ class HeatPumpLinear:
 
 
 class CHP(solph.Transformer):
-    def __init__(self, buildingLabel, input, outputEl, outputSH, outputDHW, efficiencyEl, efficiencySH, efficiencyDHW, capacityMin, CapacityEl, capacitySH, capacityDHW, epc, base):
+    def __init__(self, buildingLabel, input, outputEl, outputSH, outputDHW, efficiencyEl, efficiencySH, efficiencyDHW,
+                 capacityMin, CapacityEl, capacitySH, capacityDHW, epc, base, varc1, varc2, env_flow1, env_flow2, env_capa):
         super(CHP, self).__init__(
                         label='CHP'+'__'+buildingLabel,
                         inputs={
@@ -62,23 +73,38 @@ class CHP(solph.Transformer):
                                     maximum=CapacityEl,
                                     nonconvex=True,
                                     offset=base,
-                                )),
+                                    env_per_capa=env_capa,
+                                ),
+                                variable_costs=varc1,
+                                env_per_flow=env_flow1,
+
+                            ),
                             outputSH: solph.Flow(
                                 investment=solph.Investment(
                                     ep_costs=epc,
                                     minimum=capacityMin,
                                     maximum=capacitySH,
                                     nonconvex=True,
-                                    offset=base,
-                                )),
+                                    #offset=base,
+                                    env_per_capa=env_capa,
+                                ),
+                                variable_costs=varc2,
+                                env_per_flow=env_flow2,
+
+                            ),
                             outputDHW: solph.Flow(
                                 investment=solph.Investment(
                                     ep_costs=epc,
                                     minimum=capacityMin,
                                     maximum=capacityDHW,
                                     nonconvex=True,
-                                    offset=base,
-                                )),
+                                    #offset=base,
+                                    env_per_capa=env_capa,
+                                ),
+                                variable_costs=varc2,
+                                env_per_flow=env_flow2,
+
+                            ),
                         },
                         conversion_factors={outputEl: efficiencyEl,
                                             outputSH: efficiencySH,
--- oemof/Abstract Code/storages.py
+++ oemof/Abstract Code/storages.py
@@ -1,25 +1,20 @@
 from oemof.thermal.stratified_thermal_storage import (
     calculate_storage_u_value,
-    calculate_storage_dimensions,
-    calculate_capacities,
     calculate_losses,
 )
 
 import oemof.solph as solph
-from oemof.tools import economics
-
 
 class ElectricalStorage(solph.components.GenericStorage):
-    def __init__(self, buildingLabel, input, output, loss_rate, initial_storage, efficiency_in, efficiency_out, capacity_min, capacity_max, epc, base):
+    def __init__(self, buildingLabel, input, output, loss_rate, initial_storage, efficiency_in, efficiency_out, capacity_min, capacity_max, epc, base, varc, env_flow, env_capa):
         super(ElectricalStorage, self).__init__(
             label="electricalStorage"+'__'+buildingLabel,
             inputs={
                 input: solph.Flow()
             },
             outputs={
-                output: solph.Flow()
+                output: solph.Flow(variable_costs=varc, env_per_flow=env_flow, )
             },
-            # nominal_storage_capacity=s["nominal capacity"],
             loss_rate=loss_rate,
             initial_storage_level=initial_storage,
             inflow_conversion_factor=efficiency_in,
@@ -34,31 +29,30 @@ class ElectricalStorage(solph.components.GenericStorage):
                 existing=0,
                 nonconvex=True,
                 offset=base,
+                env_per_capa=env_capa,
             ),
         )
 
 class ThermalStorage(solph.components.GenericStorage):
-    def __init__(self, label, stratifiedStorageParams, Temperature_h, input, output, initial_storage, capacity_min, capacity_max, epc, base):
-        u_value, volume, surface, nominal_storage_capacity, loss_rate, fixed_losses_relative, \
-        fixed_losses_absolute = self._precalculate(stratifiedStorageParams, Temperature_h)
+    def __init__(self, label1, label2, stratifiedStorageParams, input, output, initial_storage, capacity_min, capacity_max, epc, base, varc, env_flow, env_capa):
+        u_value, loss_rate, fixed_losses_relative, fixed_losses_absolute = self._precalculate(stratifiedStorageParams, label2)
         super(ThermalStorage, self).__init__(
-            label=label,
+            label=label1,
             inputs={
                 input: solph.Flow(),
             },
             outputs={
-                output: solph.Flow()
+                output: solph.Flow(variable_costs=varc, env_per_flow=env_flow, )
             },
-            # nominal_storage_capacity=2000,
             loss_rate=loss_rate,
             initial_storage_level=initial_storage,
             fixed_losses_relative=fixed_losses_relative,
             fixed_losses_absolute=fixed_losses_absolute,
-            inflow_conversion_factor=stratifiedStorageParams.at[0, 'inflow_conversion_factor'],
-            outflow_conversion_factor=stratifiedStorageParams.at[0, 'outflow_conversion_factor'],
+            inflow_conversion_factor=stratifiedStorageParams.at[label2, 'inflow_conversion_factor'],
+            outflow_conversion_factor=stratifiedStorageParams.at[label2, 'outflow_conversion_factor'],
             balanced=True,
-            invest_relation_input_capacity=stratifiedStorageParams.at[0, 'inflow_conversion_factor'],
-            invest_relation_output_capacity=stratifiedStorageParams.at[0, 'outflow_conversion_factor'],
+            invest_relation_input_capacity=stratifiedStorageParams.at[label2, 'inflow_conversion_factor'],
+            invest_relation_output_capacity=stratifiedStorageParams.at[label2, 'outflow_conversion_factor'],
             investment=solph.Investment(
                 minimum=capacity_min,
                 maximum=capacity_max,
@@ -66,31 +60,22 @@ class ThermalStorage(solph.components.GenericStorage):
                 existing=0,
                 nonconvex=True,
                 offset=base,
+                env_per_capa=env_capa,
             ),
         )
 
-    def _precalculate(self, dataS, Temperature_h, Temperature_c = 15):
+    def _precalculate(self, data, label):
         u_value = calculate_storage_u_value(
-            dataS.at[0,'s_iso'],
-            dataS.at[0,'lamb_iso'],
-            dataS.at[0,'alpha_inside'],
-            dataS.at[0,'alpha_outside'])
-
-        volume, surface = calculate_storage_dimensions(
-            dataS.at[0,'height'],
-            dataS.at[0,'diameter']
-        )
-
-        nominal_storage_capacity = calculate_capacities(
-            volume,
-            Temperature_h,
-            Temperature_c)
+            data.at[label, 's_iso'],
+            data.at[label, 'lamb_iso'],
+            data.at[label, 'alpha_inside'],
+            data.at[label, 'alpha_outside'])
 
         loss_rate, fixed_losses_relative, fixed_losses_absolute = calculate_losses(
             u_value,
-            dataS.at[0,'diameter'],
-            Temperature_h,
-            Temperature_c,
-            dataS.at[0,'temp_env'])
+            data.at[label, 'diameter'],
+            data.at[label, 'temp_h'],
+            data.at[label, 'temp_c'],
+            data.at[label, 'temp_env'])
 
-        return u_value, volume, surface, nominal_storage_capacity, loss_rate, fixed_losses_relative, fixed_losses_absolute
+        return u_value, loss_rate, fixed_losses_relative, fixed_losses_absolute
