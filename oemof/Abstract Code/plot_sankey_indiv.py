0f087ca48bcc7b7fb2deef1f87ae05b297793424
jeremy.remus <jeremy.remus@ost.ch>
2021-10-18 15:57:12 +0200

    Error fixed for fixed positions

--- oemof/Abstract Code/plot_sankey_indiv.py
+++ oemof/Abstract Code/plot_sankey_indiv.py
@@ -15,31 +15,31 @@ colorDict={"elec":'rgba'+str(colors.to_rgba("skyblue",opacity)),
            }
 
 PositionDict={
-    "naturalGas"	        :	[0, 0.75],
-    "gridBus"				:	[0, 0.15],
-    "pv"					:   [0, 0.35],
-    "gridElect"				:	[0.1, 0.05],
-    "CHP_SH"				:	[0.1, 0.6],
-    "CHP_DHW"				:	[0.1, 1],
-    "electricityBus"		:	[0.2, 0.2],
-    "producedElectricity"	:	[0.4, 0.2],
-    "electricityLink"	    :	[0.4, 0.35],
-	"electricalStorage"	    :	[0.4, 0.25],
-    "excesselect"	        :	[0.5, 0.05],
-    "electricityInBus"		:	[0.5, 0.3],
-    "HP_SH"					:	[0.6, 0.35],
-    "HP_DHW"				:	[0.6, 0.9],
-    "solarCollector"		:	[0.6, 0.95],
-    "spaceHeatingBus"		:	[0.7, 0.6],
-    "spaceHeating_"			:	[0.8, 0.6],
-    "shStorage"				:	[0.8, 0.37],
-    "spaceHeatingDemandBus"	:	[0.9, 0.6],
-    "dhwStorageBus"			:	[0.7, 0.9],
-    "dhwStorage_"			:	[0.8, 0.9],
-	"domesticHotWaterBus"	:	[0.9, 0.9],
-    "electricityDemand"		:	[1, 0.2],
-    "spaceHeatingDemand_"	:	[1, 0.6],
-    "domesticHotWaterDemand":	[1, 0.9]
+    "naturalGas":	[0.001, 0.75],
+    "gridBus":	[0.001, 0.15],
+    "pv":   [0.001, 0.35],
+    "gridElect":	[0.1, 0.05],
+    "CHP_SH":	[0.1, 0.6],
+    "CHP_DHW":	[0.1, 0.99],
+    "electricityBus":	[0.2, 0.2],
+    "producedElectricity":	[0.3, 0.2],
+    "electricityLink":	[0.3, 0.35],
+	"electricalStorage":	[0.3, 0.25],
+    "excesselect":	[0.5, 0.05],
+    "electricityInBus":	[0.5, 0.2],
+    "HP_SH":	[0.6, 0.3],
+    "HP_DHW":	[0.6, 0.9],
+    "solarCollector":	[0.6, 0.95],
+    "spaceHeatingBus":	[0.7, 0.6],
+    "spaceHeating_":	[0.8, 0.6],
+    "shStorage":	[0.8, 0.37],
+    "spaceHeatingDemandBus":	[0.9, 0.6],
+    "dhwStorageBus":	[0.7, 0.9],
+    "dhwStorage_":	[0.8, 0.9],
+	"domesticHotWaterBus":	[0.9, 0.9],
+    "electricityDemand":	[0.999, 0.1],
+    "spaceHeatingDemand_":	[0.999, 0.6],
+    "domesticHotWaterDemand":	[0.999, 0.9]
 	}
 
 def readResults(fileName, buildings):
@@ -96,13 +96,12 @@ def createSankeyData(dataDict, keys, buildings=[]):
             dfKeySplit = dfKey.split("'")
             # if "domesticHotWaterDemand" in dfKeySplit[3]:
             #     continue
-            sourceNodeName=dfKeySplit[1];
-            targetNodeName =dfKeySplit[3];
+            sourceNodeName=dfKeySplit[1]
+            targetNodeName =dfKeySplit[3]
             if "Resource" not in sourceNodeName:
                 dfKeyValues = df[dfKey].values
                 value = sum(dfKeyValues)
                 if value < 1:
-                    value = 0
                     continue
                 values.append(value)
                 if sourceNodeName not in nodes:
@@ -112,12 +111,12 @@ def createSankeyData(dataDict, keys, buildings=[]):
                     for posKey in PositionDict.keys():
                         if posKey in sourceNodeName and posKey[0:2] == sourceNodeName[0:2]: #second part of the term added for CHP and HP
                             x.append(PositionDict[posKey][0])
-                            a=sourceNodeName[-1]
                             if "electricityLink" in sourceNodeName:
-                                y.append(0.5-PositionDict[posKey][1])
+                                y.append(0.5-(PositionDict[posKey][1]))
                             else:
                                 buildingNumber=buildings.index(int(sourceNodeName[-1]))
-                                y.append(PositionDict[posKey][1]/len(buildings)*(buildingNumber)*0.5)
+                                temp = (PositionDict[posKey][1]) / len(buildings) + (buildingNumber) / len(buildings)
+                                y.append(temp)
                 sources.append(nodes.index(sourceNodeName))
 
                 if targetNodeName not in nodes:
@@ -127,12 +126,12 @@ def createSankeyData(dataDict, keys, buildings=[]):
                     for posKey in PositionDict.keys():
                         if posKey in targetNodeName and posKey[0:2] == targetNodeName[0:2]:
                             x.append(PositionDict[posKey][0])
-                            a = targetNodeName[-1]
                             if "electricityLink" in targetNodeName:
-                                y.append(0.5-PositionDict[posKey][1])
+                                y.append(0.5-(PositionDict[posKey][1]))
                             else:
                                 buildingNumber=buildings.index(int(targetNodeName[-1]))
-                                y.append(PositionDict[posKey][1]/len(buildings)+(buildingNumber)*0.5)
+                                temp = (PositionDict[posKey][1]) / len(buildings) + (buildingNumber) / len(buildings)
+                                y.append(temp)
                 targets.append(nodes.index(targetNodeName))
     return nodes, sources, targets, values, x, y, linkGroup
 
