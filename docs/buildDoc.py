71adddcfa1bf0d4b07e3178ac1ba7a17f7082c25
Neha Dimri <neha.dimri@ost.ch>
2022-06-27 16:42:54 +0200

    documentation

--- docs/buildDoc.py
+++ docs/buildDoc.py
@@ -4,7 +4,7 @@
 import os
 
 os.system("sphinx-apidoc -f -o . ../optihood")
-os.system('make clean')
+# os.system('make clean')
 os.system("make html")
 # os.system('make pdf')
 # os.system('sphinx-build -b pdf ../optihood')
--- docs/conf.py
+++ docs/conf.py
@@ -97,8 +97,6 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_extra_path = ["source\\googlefc5d33f8dcbdfe4b.html"]
-
 source_suffix = {
     ".rst": "restructuredtext",
     ".txt": "markdown",
