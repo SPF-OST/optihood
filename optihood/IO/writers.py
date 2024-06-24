e9be591132ce01be320ec1fcb4d87b4c6e90ce9c
ahobeost <ahobeost@github.com>
2024-06-24 17:09:26 +0200

    Adjusted version of _pd.ExcelWriter as old version does not seem to be supported anymore.

--- optihood/IO/writers.py
+++ optihood/IO/writers.py
@@ -3,8 +3,6 @@ import pandas as _pd
 
 def write_prepared_data_and_sheets_to_excel(excel_file_path: str, excel_data: dict):
     # better to use pathlib paths.
-    with _pd.ExcelWriter(excel_file_path, engine='xlwt') as writer:
+    with _pd.ExcelWriter(excel_file_path, engine='openpyxl') as writer:
         for sheet, data in excel_data.items():
             data.to_excel(writer, sheet_name=sheet, index=False)
-        writer.save()
-        writer.close()  # is this needed when using "with"?
