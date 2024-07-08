b72337b19539caed18819de5af506f61c0e8a095
ahobeost <ahobeost@github.com>
2024-07-08 11:08:23 +0200

    output extension change in test needed as well.

--- tests/test_examples/test_xls_examples.py
+++ tests/test_examples/test_xls_examples.py
@@ -90,7 +90,7 @@ class TestXlsExamples(_ut.TestCase):
         _os.chdir(cwd)
         # =============================
 
-        excel_file_path = str(example_path / "results_basic_example.xls")
+        excel_file_path = str(example_path / "results_basic_example.xlsx")
         expected_data_path = str(expected_data_dir / "test_results_basic_example_after_merge.xls")
 
         compare_xls_files(self, excel_file_path, expected_data_path, _SHEET_NAMES, abs_tolerance=1e-4)
