60431e37a1eb12d5829613eb8303ebf81a71b220
ahobeost <ahobeost@github.com>
2024-07-08 11:25:58 +0200

    Initial tests for nodal inputs

--- tests/test_energy_network.py
+++ tests/test_energy_network.py
@@ -5,20 +5,19 @@ import pandas as _pd
 
 import optihood as _oh
 import optihood.energy_network as _en
-from tests.xls_helpers import check_assertion
 
 cwd = _os.getcwd()
 packageDir = _pl.Path(_oh.__file__).resolve().parent
 _input_data_dir = packageDir / ".." / "data" / "excels" / "basic_example"
 _examples_dir = packageDir / ".." / "data" / "examples"
+_input_data_path = str(_input_data_dir / "scenario.xls")
 
 
 class TestEnergyNetwork:
     def test_set_from_excel(self):
-        """ File System touching test to check see whether network is populated correctly using a scenario. """
+        """File System touching test to check see whether network is populated correctly using a scenario."""
 
         # Given
-        _input_data_path = str(_input_data_dir / "scenario.xls")
 
         time_period = _pd.date_range("2018-01-01 00:00:00", "2018-01-31 23:00:00", freq="60min")
         nr_of_buildings = 4
@@ -44,11 +43,52 @@ class TestEnergyNetwork:
 
         check_assertion(errors, len(network.timeincrement), 744)
 
-        check_assertion(errors, network.timeincrement.unique(), 1.)
+        check_assertion(errors, network.timeincrement.unique(), 1.0)
 
         check_assertion(errors, len(network.timeindex), 745)
 
         if errors:
             raise ExceptionGroup(f"found {len(errors)} errors", errors)
 
+    def test_get_nodal_data_from_Excel(self):
+        network = _en.EnergyNetworkClass(None)
+        _os.chdir(_examples_dir)
+        data = _pd.ExcelFile(_input_data_path)
+        initial_nodal_data = network.get_nodal_data_from_Excel(data)
 
+        errors = []
+
+        try:
+            assert len(initial_nodal_data) == 9
+        except AssertionError as current_error:
+            errors.append(current_error)
+
+        try:
+            assert list(initial_nodal_data.keys()) == [
+                "buses",
+                "grid_connection",
+                "commodity_sources",
+                "solar",
+                "transformers",
+                "demand",
+                "storages",
+                "stratified_storage",
+                "profiles",
+            ]
+        except AssertionError as current_error:
+            errors.append(current_error)
+
+        try:
+            assert initial_nodal_data["buses"].shape == (48, 5)
+        except AssertionError as current_error:
+            errors.append(current_error)
+
+        if errors:
+            raise ExceptionGroup(f"found {len(errors)} errors", errors)
+
+
+def check_assertion(errors: list, actual, expected):
+    try:
+        assert actual == expected
+    except AssertionError as current_error:
+        errors.append(current_error)
