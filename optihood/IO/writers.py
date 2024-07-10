c5fe96fdc8e17b1e6d6cc7b439be748a618b36d6
ahobeost <ahobeost@github.com>
2024-07-10 16:53:55 +0200

    Added writer to config example.

--- optihood/IO/writers.py
+++ optihood/IO/writers.py
@@ -22,35 +22,44 @@ class ScenarioCreator:
 
     def __post_init__(self):
         if self.version == 'grouped':
-            self.get_scenario = self.get_grouped_scenario
+            self.get_scenario = self._get_grouped_scenario
         elif self.version == 'individual':
-            self.get_scenario = self.get_individual_scenario
+            self.get_scenario = self._get_individual_scenario
 
-    def get_grouped_scenario(self):
+    def _get_grouped_scenario(self):
         data = _gsw.create_scenario_file(self.config_file_path, self.nr_of_buildings)
         return data
 
-    def get_individual_scenario(self):
+    def _get_individual_scenario(self):
         data = _isw.create_scenario_file(self.config_file_path, self.building_nrs, self.nr_of_buildings)
         return data
 
     @_abc.abstractmethod
-    def write_scenario_to_file(self, file_path: _pl.Path) -> None:
+    def _write_scenario_to_file(self, file_path: _pl.Path) -> None:
         pass
 
     def write(self, file_path: _pl.Path) -> None:
         self.data = self.get_scenario()
-        self.write_scenario_to_file(file_path)
+        self._write_scenario_to_file(file_path)
 
 
 class ScenarioFileWriterExcel(ScenarioCreator):
-    def write_scenario_to_file(self, file_path: _pl.Path) -> None:
-        # maybe this can be inlined?
+    def _write_scenario_to_file(self, file_path: _pl.Path) -> None:
         write_prepared_data_and_sheets_to_excel(file_path, self.data)
 
 
+def write_prepared_data_and_sheets_to_CSVs(file_path, data):
+    pass
+
+
+class ScenarioFileWriterCSV(ScenarioCreator):
+    def _write_scenario_to_file(self, file_path: _pl.Path) -> None:
+        # maybe this can be inlined?
+        write_prepared_data_and_sheets_to_CSVs(file_path, self.data)
+
+
 def write_prepared_data_and_sheets_to_excel(excel_file_path: _pl.Path, excel_data: dict):
-    # better to use pathlib paths.
+    # maybe this can be inlined?
     with _pd.ExcelWriter(excel_file_path, engine='openpyxl') as writer:
         for sheet, data in excel_data.items():
             data.to_excel(writer, sheet_name=sheet, index=False)
