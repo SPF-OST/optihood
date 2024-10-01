cac025b3bb3ee044c4daf463d6bcffe9d3828269
ahobeost <ahobeost@github.com>
2024-10-01 11:27:03 +0200

    Adjusted cytoscape example to use polymorphic classes.

--- optihood/Visualizer/visualizer_app.py
+++ optihood/Visualizer/visualizer_app.py
@@ -1,6 +1,8 @@
 import json
 from textwrap import dedent as d
 import typing as _tp
+import dataclasses as _dc
+import enum as _enum
 
 import dash
 import plotly.express as px
@@ -8,10 +10,39 @@ from dash import html, dcc, Input, Output, State, callback
 import dash_cytoscape as cyto
 import matplotlib.pyplot as plt
 
-app = dash.Dash('Optihood input Visualizer')
+
+@_dc.dataclass()
+class NodalDataExample:
+    id: str
+    label: str
+    longitude: float
+    latitude: float
+
+    def get_nodal_infos(self):
+        return {
+            'data': {'id': self.id, 'label': self.label, "lat": self.latitude, "long": self.longitude},
+            'position': {'x': 20 * self.latitude, 'y': -20 * self.longitude}
+        }
+
+    @staticmethod
+    def read_nodal_infos(data: dict[str, _tp.Union[str, float, int]]):
+        return f"{data['label']}, {data['lat']}, {data['long']}"
 
 
-def setup_cytoscape_app(nodes: _tp.Dict[str, _tp.Dict[str, _tp.Union[str, float]]] , edges) -> dash.Dash:
+class NodalDataTypes(_enum.StrEnum):
+    example: str = 'example'
+
+
+def nodal_data_factory(nodal_data_type: str):
+    nodal_data_types = {NodalDataTypes.example: NodalDataExample}
+
+    if nodal_data_type not in nodal_data_types:
+        raise NotImplementedError("received unexpected type")
+
+    return nodal_data_types[nodal_data_type]
+
+
+def setup_cytoscape_app(nodes: _tp.Dict[str, _tp.Dict[str, _tp.Union[str, float]]], edges) -> dash.Dash:
     """
     This example comes directly from the Plotly homepage.
     http://dash.plotly.com/cytoscape/events
@@ -27,6 +58,7 @@ def setup_cytoscape_app(nodes: _tp.Dict[str, _tp.Dict[str, _tp.Union[str, float]
         {
             'selector': 'node',
             'style': {
+                # 'background-color': '#BFD7B5',
                 'label': 'data(label)'
             }
         },
@@ -46,13 +78,70 @@ def setup_cytoscape_app(nodes: _tp.Dict[str, _tp.Dict[str, _tp.Union[str, float]
     ])
 
     @callback(Output('cytoscape-selectedNodeData-markdown', 'children'),
-          Input('cytoscape-event-callbacks-3', 'selectedNodeData'))
+              Input('cytoscape-event-callbacks-3', 'selectedNodeData'))
     def displaySelectedNodeData(data_list):
         if data_list is None:
             return "No nodes selected."
 
-        selected_nodes_list = [f"{data['label']}, {data['lat']}, {data['long']}" for data in data_list]
+        NodalData = nodal_data_factory(NodalDataTypes.example)
+        selected_nodes_list = [NodalData.read_nodal_infos(data) for data in data_list]
         return "You selected the nodes: " + "\n* ".join(selected_nodes_list)
+
+    @callback(Output('cytoscape-event-callbacks-3', 'elements'),
+              Input('btn-add-node-example', 'n_clicks_timestamp'),
+              Input('btn-remove-node-example', 'n_clicks_timestamp'),
+              State('cytoscape-event-callbacks-3', 'elements'))
+    def update_elements(btn_add, btn_remove, elements):
+        current_nodes, deleted_nodes = get_current_and_deleted_nodes(elements)
+        # If the add button was clicked most recently and there are nodes to add
+        if int(btn_add) > int(btn_remove) and len(deleted_nodes):
+
+            # We pop one node from deleted nodes and append it to nodes list.
+            current_nodes.append(deleted_nodes.pop())
+            # Get valid edges -- both source and target nodes are in the current graph
+            cy_edges = get_current_valid_edges(current_nodes, edges)
+            return cy_edges + current_nodes
+
+        # If the remove button was clicked most recently and there are nodes to remove
+        elif int(btn_remove) > int(btn_add) and len(current_nodes):
+            current_nodes.pop()
+            cy_edges = get_current_valid_edges(current_nodes, edges)
+            return cy_edges + current_nodes
+
+        # Neither have been clicked yet (or fallback condition)
+        return elements
+
+    def get_current_valid_edges(current_nodes, all_edges):
+        """Returns edges that are present in Cytoscape:
+        its source and target nodes are still present in the graph.
+        """
+        valid_edges = []
+        node_ids = {n['data']['id'] for n in current_nodes}
+
+        for e in all_edges:
+            if e['data']['source'] in node_ids and e['data']['target'] in node_ids:
+                valid_edges.append(e)
+        return valid_edges
+
+    def get_current_and_deleted_nodes(elements):
+        """Returns nodes that are present in Cytoscape and the deleted nodes
+        """
+        current_nodes = []
+        deleted_nodes = []
+
+        # get current graph nodes
+        for ele in elements:
+            # if the element is a node
+            if 'source' not in ele['data']:
+                current_nodes.append(ele)
+
+        # get deleted nodes
+        node_ids = {n['data']['id'] for n in current_nodes}
+        for n in nodes:
+            if n['data']['id'] not in node_ids:
+                deleted_nodes.append(n)
+
+        return current_nodes, deleted_nodes
 
     return app
 
@@ -80,7 +169,7 @@ def setup_plotly_app(figure_handle: plt.Figure) -> dash.Dash:
             html.Div([
                 dcc.Markdown(d("""
                     **Hover Data**
-    
+
                     Mouse over values in the graph.
                 """)),
                 html.Pre(id='hover-data', style=styles['pre'])
@@ -89,7 +178,7 @@ def setup_plotly_app(figure_handle: plt.Figure) -> dash.Dash:
             html.Div([
                 dcc.Markdown(d("""
                     **Click Data**
-    
+
                     Click on points in the graph.
                 """)),
                 html.Pre(id='click-data', style=styles['pre']),
@@ -98,11 +187,11 @@ def setup_plotly_app(figure_handle: plt.Figure) -> dash.Dash:
             html.Div([
                 dcc.Markdown(d("""
                     **Selection Data**
-    
+
                     Choose the lasso or rectangle tool in the graph's menu
                     bar and then select points in the graph.
-    
-                    Note that if `layout.clickmode = 'event+select'`, selection data also 
+
+                    Note that if `layout.clickmode = 'event+select'`, selection data also
                     accumulates (or un-accumulates) selected data if you hold down the shift
                     button while clicking.
                 """)),
@@ -112,7 +201,7 @@ def setup_plotly_app(figure_handle: plt.Figure) -> dash.Dash:
             html.Div([
                 dcc.Markdown(d("""
                     **Zoom and Relayout Data**
-    
+
                     Click and drag on the graph to zoom or click on the zoom
                     buttons in the graph's menu bar.
                     Clicking on legend items will also fire
@@ -161,28 +250,27 @@ def run_cytoscape_visualizer(nodes, edges) -> None:
 
 
 if __name__ == '__main__':
-    version = "plotly"
+
+    version = "cytoscape"
     if version == "plotly":
         df = px.data.iris()
         fig = px.scatter_matrix(df[['sepal_length', 'sepal_width']])
         run_fig_visualizer(fig)
     elif version == "cytoscape":
-        nodes_dict = [
-            {
-                'data': {'id': short, 'label': label, "lat": lat, "long": long},
-                'position': {'x': 20 * lat, 'y': -20 * long}
-            }
-            for short, label, long, lat in (
-                ('la', 'Los Angeles', 34.03, -118.25),
-                ('nyc', 'New York', 40.71, -74),
-                ('to', 'Toronto', 43.65, -79.38),
-                ('mtl', 'Montreal', 45.50, -73.57),
-                ('van', 'Vancouver', 49.28, -123.12),
-                ('chi', 'Chicago', 41.88, -87.63),
-                ('bos', 'Boston', 42.36, -71.06),
-                ('hou', 'Houston', 29.76, -95.37)
-            )
+        NodalData = nodal_data_factory(NodalDataTypes.example)
+        nodal_data_list = [
+            NodalData('la', 'Los Angeles', 34.03, -118.25),
+            NodalData('nyc', 'New York', 40.71, -74),
+            NodalData('to', 'Toronto', 43.65, -79.38),
+            NodalData('mtl', 'Montreal', 45.50, -73.57),
+            NodalData('van', 'Vancouver', 49.28, -123.12),
+            NodalData('chi', 'Chicago', 41.88, -87.63),
+            NodalData('bos', 'Boston', 42.36, -71.06),
+            NodalData('hou', 'Houston', 29.76, -95.37)
         ]
+        nodes_dict = []
+        for i, nodalData in enumerate(nodal_data_list):
+            nodes_dict.append(nodalData.get_nodal_infos())
 
         edges_dict = [
             {'data': {'source': source, 'target': target}}
@@ -199,4 +287,5 @@ if __name__ == '__main__':
                 ('nyc', 'bos')
             )
         ]
+        app = dash.Dash('Optihood input Visualizer')
         run_cytoscape_visualizer(nodes_dict, edges_dict)
