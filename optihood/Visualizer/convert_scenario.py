23b444f0ccc3f6547d2a49c6114c4b611459edb0
ahobeost <ahobeost@github.com>
2024-10-02 08:58:18 +0200

    Adjusted convert_scenario

--- optihood/Visualizer/convert_scenario.py
+++ optihood/Visualizer/convert_scenario.py
@@ -8,6 +8,7 @@ import optihood.Visualizer.scenario_to_visualizer as _stv
 
 @_dc.dataclass()
 class EnergyNetworkGraphData:
+    """ This may grow further. """
     nodes: _abc.Sequence[dict[str, dict[str, _tp.Union[str, float, int]]]]
     edges: _abc.Sequence[dict[str, dict[str, _tp.Union[str, float, int]]]]
 
@@ -20,10 +21,19 @@ def get_converters(initial_nodal_data: dict[str, _pd.DataFrame]) -> _abc.Sequenc
         # ========================================================================
         """ This needs to be removed after all current sheets are implemented. """
         if not converter:
+            print(f"{sheet_name} does not have a converter yet.")
             continue
         # ========================================================================
+        converters += converter.set_from_dataFrame(sheet)
 
+    return converters
 
 
 def get_graph_data(converters: _abc.Sequence[_stv.ScenarioToVisualizerAbstract]) -> EnergyNetworkGraphData:
-    pass
+    nodes = [converter.get_nodal_infos() for converter in converters]
+
+    edges = []
+    for converter in converters:
+        edges += converter.get_edge_infos()
+
+    return EnergyNetworkGraphData(nodes, edges)
