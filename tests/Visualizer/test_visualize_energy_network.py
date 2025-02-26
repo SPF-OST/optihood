8780d2e0a9d2cc076aa340ee992eeed6b57e3f82
Neha Dimri <83694401+nehadimri1991@users.noreply.github.com>
2025-02-26 13:17:37 +0100

    test: option to filter nodes by building number

--- tests/Visualizer/test_visualize_energy_network.py
+++ tests/Visualizer/test_visualize_energy_network.py
@@ -2,23 +2,35 @@ import json
 import os as _os
 import pathlib as _pl
 import unittest as _ut
-import pytest as _pt
-import json as _json
 
 import pandas as _pd
+import pytest as _pt
 
 import optihood as _oh
-import optihood.energy_network as _en
-import optihood.Visualizer.visualizer_app as _va
+import optihood.IO.readers as _re
 import optihood.Visualizer.convert_scenario as _cs
+import optihood.Visualizer.visualizer_app as _va
+import optihood.energy_network as _en
 
 cwd = _os.getcwd()
 packageDir = _pl.Path(_oh.__file__).resolve().parent
 _input_data_dir = packageDir / ".." / "data" / "excels" / "basic_example"
 _examples_dir = packageDir / ".." / "data" / "examples"
 _input_data_path = _input_data_dir / "scenario.xls"
+filter_results = False   #False
+include_links = True
+building_no = 1
 
-manual = True
+manual = True          #True
+
+# Helper functions
+def get_building_no(node):
+    """Extract building number from node or return 0 if not present."""
+    return int(node.split("_B")[-1]) if "_B" in node else 0
+
+def is_link(node):
+    """Check if the node ends with 'link' (case-insensitive)."""
+    return node.lower().endswith("link")
 
 
 @_pt.mark.skipif(manual, reason="Manual test of visualizer")
@@ -40,10 +52,53 @@ class TestVisualizeEnergyNetwork(_ut.TestCase):
         # When
         converters = _cs.get_converters(initial_nodal_data, nr_of_buildings=4)
         graphData = _cs.get_graph_data(converters)
+        if filter_results:
+            graphData.nodes = [n for n in graphData.nodes if
+                               ('building' in n['data'] and n['data']['building'] == building_no)
+                               or (include_links and n['classes'] == "link")]
+            graphData.edges = [e for e in graphData.edges if
+                               (get_building_no(e['data']['source']) == get_building_no(e['data']['target']) == building_no)
+                               or (include_links and (is_link(e['data']['source']) or is_link(e['data']['target'])) and
+                                   (
+                                           get_building_no(e['data']['target']) == building_no or
+                                           get_building_no(e['data']['source']) == building_no
+                                   ))]
         _va.run_cytoscape_visualizer(graphData=graphData)
 
         self.assertEqual(True, False)  # add assertion here
 
+
+@_pt.mark.skipif(manual, reason="Manual test of visualizer")
+class TestVisualizeEnergyNetwork_from_csv(_ut.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+
+    def test_visualize_from_energy_network(self):
+        # Given
+        input_data_dir = _pl.Path("C:\Daten\OngoingProjects\gisoptites\GISoptiTES\data\optihood_scenarios\scenario_3")
+
+        _os.chdir(_examples_dir)
+        csvReader = _re.CsvScenarioReader(input_data_dir)
+        initial_nodal_data = csvReader.read_scenario()
+        _os.chdir(cwd)
+
+        # When
+        converters = _cs.get_converters(initial_nodal_data, nr_of_buildings=4)
+        graphData = _cs.get_graph_data(converters)
+        if filter_results:
+            graphData.nodes = [n for n in graphData.nodes if
+                               ('building' in n['data'] and n['data']['building'] == building_no)
+                               or (include_links and n['classes'] == "link")]
+            graphData.edges = [e for e in graphData.edges if
+                               (get_building_no(e['data']['source']) == get_building_no(e['data']['target']) == building_no)
+                               or (include_links and (is_link(e['data']['source']) or is_link(e['data']['target'])) and
+                                   (
+                                           get_building_no(e['data']['target']) == building_no or
+                                           get_building_no(e['data']['source']) == building_no
+                                   ))]
+        _va.run_cytoscape_visualizer(graphData=graphData)
+
+        self.assertEqual(True, False)  # add assertion here
 
 class TestScenarioTillGraphData(_ut.TestCase):
     def setUp(self):
