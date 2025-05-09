491194753b8cb9a4ae2c5de12589d002bc407fa2
ahobeost <ahobeost@github.com>
2025-05-09 11:20:58 +0200

    Adjusted get_current_system_state to be reproducible.

--- data/examples/MPC_example.py
+++ data/examples/MPC_example.py
@@ -8,6 +8,16 @@ from optihood.MPC.interface import MpcHandler
 from optihood.energy_network import OptimizationProperties
 
 
+def get_current_system_state(system_state: dict[str, float]) -> dict[str, float]:
+    """Required function to interface with real/virtual system.
+    As such a system is not the focus of this example, we will update the system using random numbers.
+    In a real case, the User will need to connect the real/virtual system here.
+    """
+    system_state['electricalStorage__B001']['initial capacity'] = round(_np.random.random(), 2)
+    system_state['shStorage__B001']['initial capacity'] = round(_np.random.random(), 2)
+    return system_state
+
+
 def translate_flows_to_control_signals(energy_flows):
     raise NotImplementedError
 
@@ -20,17 +30,8 @@ def control_system(control_signals):
     pass
 
 
-def get_current_system_state(system_state: dict[str, float]) -> dict[str, float]:
-    """Required function to interface with real/virtual system.
-    As such a system is not the focus of this example, we will update the system using random numbers.
-    In a real case, the User will need to connect the real/virtual system here.
-    """
-    system_state['electricalStorage__B001'] = _np.random.random()
-    system_state['shStorage__B001'] = _np.random.random()
-    return system_state
-
-
 if __name__ == '__main__':
+    # TODO: produce a test for this example.
     # Would you like to visualize the energy network?
     visualize = True
     # This will abort the MPC simulation after visualizing.
@@ -43,18 +44,19 @@ if __name__ == '__main__':
     current_dir = _pl.Path(__file__).resolve().parent
     input_folder_path = current_dir / ".." / "CSVs" / "MPC_example_CSVs"
 
-    # TODO adjust this
-    resultFilePath = current_dir / ".." / "results"
-    resultFileName = "results_MPC_example"
+    result_dir_path = current_dir / ".." / "results"
+    result_file_name = "results_MPC_example"
 
     # initialize parameters
     numberOfBuildings = 1
 
     # We will use 4 time steps of 1 hour
-    example_time_steps = _pd.date_range("2018-01-01 00:00:00", "2018-01-01 04:00:00", freq=f"{str(time_step_in_minutes)}min")
+    example_time_steps = _pd.date_range("2018-01-01 00:00:00", "2018-01-01 04:00:00",
+                                        freq=f"{str(time_step_in_minutes)}min")
 
     # The MpcHandler will take care of many things for us.
-    mpc = MpcHandler(prediction_window_in_hours=prediction_window_in_hours, time_step_in_minutes=time_step_in_minutes, nr_of_buildings=1)
+    mpc = MpcHandler(prediction_window_in_hours=prediction_window_in_hours, time_step_in_minutes=time_step_in_minutes,
+                     nr_of_buildings=1)
 
     # We set the optimization properties, that would normally be given to the Network directly.
     # TODO: figure out the rest of the inputs for this example.
@@ -77,13 +79,16 @@ if __name__ == '__main__':
     # {'electricalStorage__B001': {'initial capacity': 0},
     #  'shStorage__B001': {'initial capacity': 0}
     #  }
+    # This can be used to define the function "get_current_system_state"
 
     if visualize:
         mpc.visualize_example()
         exit()
 
-    if not _os.path.exists(resultFilePath):
-        _os.makedirs(resultFilePath)
+    if not _os.path.exists(result_dir_path):
+        _os.makedirs(result_dir_path)
+
+    _np.random.seed(0)  # ensure reproducibility of results
 
     for current_time_step in example_time_steps:
         # ===============
@@ -94,16 +99,17 @@ if __name__ == '__main__':
         network = mpc.update_network(current_time_step, current_state)
 
         _, _, _ = network.optimize(solver='gurobi', numberOfBuildings=numberOfBuildings)
+        # results, optimization_meta_data = network.optimize(solver='gurobi', numberOfBuildings=numberOfBuildings)
+        # return dataclass
+        results = network.saveUnprocessedResults()
 
         # ========================================================================
         # logged automatically?
-        mpc.log_processing(network, costs=True, env_impacts=True, meta=True)
+        mpc.log_processing(results, costs=True, env_impacts=True, meta=True)
         # ========================
 
-        # TODO: adjust name to time step.
-        result_file_path = resultFilePath / f"{resultFileName}_{current_time_step.strftime("%Y_%m_%d__%H_%M_%S")}.xlsx"
+        result_file_path = result_dir_path / f"{result_file_name}_{current_time_step.strftime("%Y_%m_%d__%H_%M_%S")}.xlsx"
         network.exportToExcel(result_file_path)
-        results = network.saveUnprocessedResults()
 
         control_signals = mpc.get_desired_control_signals(results)
 
