4cef5410c7d39554b54641c4ab3654f9bb53c5af
ahobeost <ahobeost@github.com>
2025-05-15 10:55:55 +0200

    Added define_desired_flows_and_their_new_names and translate_flows_to_control_signals with extensive explanations.

--- data/examples/MPC_example.py
+++ data/examples/MPC_example.py
@@ -1,3 +1,14 @@
+"""
+This example shows how to use the MPC interface.
+After the imports, there are a few example functions, which the User should provide:
+get_current_system_state
+define_desired_flows_and_their_new_names
+translate_flows_to_control_signals
+control_system
+"""
+
+# ================
+# required imports
 import pathlib as _pl
 
 import pandas as _pd
@@ -7,6 +18,12 @@ from optihood.MPC.interface import MpcHandler
 from optihood.energy_network import OptimizationProperties
 
 
+# required imports
+# ================
+
+
+# =========================
+# functions the User should provide
 def get_current_system_state(system_state: dict[str, dict[str, float]]) -> dict[str, dict[str, float]]:
     """Required function to interface with real/virtual system.
     As such a system is not the focus of this example, we will update the system using random numbers.
@@ -17,13 +34,84 @@ def get_current_system_state(system_state: dict[str, dict[str, float]]) -> dict[
     return system_state
 
 
+def define_desired_flows_and_their_new_names():
+    """The MPC will predict flows for every time step in the predicted window.
+    Only a subset of those flows will be needed to control the system.
+
+    The names of these flows is very cumbersome, so renaming them here makes life easier when
+    translating the flows to control signals.
+    {"HP__B001__To__shSourceBus__B001": "HP_out",
+    "shSourceBus__B001__To__shStorage__B001": "HP_into_storage"}
+
+    To keep the same name, just use it twice:
+    {"HP__B001__To__shSourceBus__B001": "HP__B001__To__shSourceBus__B001"}
+    """
+    return {
+        # ================================
+        # electrical part till heat pump (HP)
+        # --------------------------------
+        # electricity produced by photovoltaics (pv)
+        "pv__B001__To__electricityProdBus__B001": "el_pv_produced",
+
+        # surplus PV electricity exported to the grid
+        "electricityBus__B001__To__excess_electricityBus": "el_to_grid",
+
+        # PV electricity stored in the battery
+        "electricityProdBus__B001__To__electricalStorage__B001": "el_pv_to_battery",
+
+        # electricity provided by the battery for own consumption
+        "electricalStorage__B001__To__electricityBus__B001": "el_battery_discharge",
+
+        # PV + battery electricity used for own consumption in this timestep
+        "electricityBus__B001__To__producedElectricity__B001": "el_produced",
+
+        # electricity taken from the grid for own consumption in this timestep
+        "electricity_resource__B001__To__gridBus__B001": "el_from_grid",
+
+        # electricity flowing into the heat pump
+        "electricityInBus__B001__To__HP__B001": "HP_el_in",
+        # ------------------------------------
+        # electrical part till heat pump (HP)
+        # ====================================
+
+        # ===================================
+        # heat part starting at the heat pump
+        # -----------------------------------
+        # full heat flow out of the heat pump
+        "HP__B001__To__shSourceBus__B001": "HP_heat_out",
+
+        # part of "HP_out" going into the storage
+        "shSourceBus__B001__To__shStorage__B001": "HP_to_TES",
+
+        # part of "HP_out" providing heat directly
+        "shSourceBus__B001__To__spaceHeatingBus__B001": "HP_to_demand",
+
+        # heat demand supplied by the storage
+        "shStorage__B001__To__spaceHeatingBus__B001": "TES_to_demand",
+
+        # space heating delivered
+        "spaceHeatingBus__B001__To__spaceHeating__B001": "sh_delivered",
+        # -----------------------------------
+        # heat part starting at the heat pump
+        # ===================================
+    }
+
+
 def translate_flows_to_control_signals(energy_flows):
     """Required function to interface with real/virtual system.
     The MPC returns energy flows between components.
     These need to be translated to e.g. pumping rates, valve positions, etc.
     For this example, we will ignore this step.
     """
-    control_signals = {}
+    control_signals = _pd.DataFrame(0, index=energy_flows.index)
+
+    # Get a True/False boolean for each time step, where the HP produces heat.
+    HP_out = energy_flows["HP_out"] > 1e-4
+
+    # Apply this on both the HP and the pump supplying fluid to the HP.
+    control_signals["HP_is_on"] = HP_out
+    control_signals["HP_pump_is_on"] = HP_out
+
     return control_signals
 
 
@@ -35,17 +123,24 @@ def control_system(control_signals):
     pass
 
 
+# functions the User should provide
+# =========================
+
+# =========================
 # define paths for input and result files
 current_dir = _pl.Path(__file__).resolve().parent
 input_folder_path = current_dir / ".." / "CSVs" / "MPC_example_CSVs"
 
 result_dir_path = current_dir / ".." / "results" / "MPC_example"
 result_file_name = "results_MPC_example"
+# define paths for input and result files
+# =========================
 
-
+# =========================
+# Usage of the MPC interface, which uses the above.
 if __name__ == '__main__':
     # Would you like to visualize the energy network?
-    visualize = False
+    visualize = True
     # This will abort the MPC simulation after visualizing.
 
     # set a time period for the optimization problem
@@ -85,6 +180,9 @@ if __name__ == '__main__':
     #  }
     # This can be used to define the function "get_current_system_state"
 
+    # The MPC will output energy flows. Here, we decide which ones we need and what to call them.
+    desired_flows_with_new_names = define_desired_flows_and_their_new_names()
+
     if visualize:
         mpc.visualize_example()
         exit()
@@ -113,14 +211,11 @@ if __name__ == '__main__':
         result_file_path = result_dir_path / f"{result_file_name}_{current_time_step.strftime("%Y_%m_%d__%H_%M_%S")}.xlsx"
         network.exportToExcel(result_file_path)
 
-        # energy_flows = mpc.get_desired_energy_flows(results)
-        energy_flows = _pd.DataFrame()
+        # energy_flows = mpc.get_desired_energy_flows(results, desired_flows_with_new_names)
+        # energy_flows = _pd.DataFrame()
 
         # ===============
         # responsibility of the User.
-        control_signals = translate_flows_to_control_signals(energy_flows)
-        control_system(control_signals)
+        # control_signals = translate_flows_to_control_signals(energy_flows)
+        # control_system(control_signals)
         # ===============
-
-
-
