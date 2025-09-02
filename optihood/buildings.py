3938b4435da98641c022d3bbbc2aa8d2a5375f41
Neha Dimri <83694401+nehadimri1991@users.noreply.github.com>
2025-09-02 11:49:05 +0200

    refactor: revisions based on suggestions by reviewer

--- optihood/buildings.py
+++ optihood/buildings.py
@@ -480,30 +480,30 @@ class Building:
                             )
                         )
 
-    def _addHeatPump(self, data, operationTempertures, temperatureAmb, opt, mergeLinkBuses, mergeHeatSourceSink, dispatchMode, temperatureLevels):
+    def _addHeatPump(self, data, operationTempertures, temperature_evap, opt, mergeLinkBuses, mergeHeatSourceSink, dispatchMode, temperatureLevels):
         label = LabelStringManipulator(data[_ent.TransformerLabels.label] + '__' + self.__buildingLabel)
-        from_bus_identifier = _ent.TransformerLabels.from_bus
-        to_bus_identifier = _ent.TransformerLabels.to
-        if mergeLinkBuses and data[from_bus_identifier] in self.__linkBuses:
-            inputBusLabel = [data[from_bus_identifier]]
-        elif mergeLinkBuses and any([b in self.__linkBuses for b in data[from_bus_identifier].split(',')]):
-            inputBusLabel = [i + '__' + self.__buildingLabel for i in data[from_bus_identifier].split(",") if
+        from_bus = _ent.TransformerLabels.from_bus
+        to_bus = _ent.TransformerLabels.to
+        if mergeLinkBuses and data[from_bus] in self.__linkBuses:
+            inputBusLabel = [data[from_bus]]
+        elif mergeLinkBuses and any([b in self.__linkBuses for b in data[from_bus].split(',')]):
+            inputBusLabel = [i + '__' + self.__buildingLabel for i in data[from_bus].split(",") if
                              i not in self.__linkBuses]
-            inputBusLabel.extend([i for i in data[from_bus_identifier].split(",") if i in self.__linkBuses])
-        elif mergeHeatSourceSink and any([b in self.__heatSourceSinkBuses for b in data[from_bus_identifier].split(',')]):
-            inputBusLabel = [i + '__' + self.__buildingLabel for i in data[from_bus_identifier].split(",") if
+            inputBusLabel.extend([i for i in data[from_bus].split(",") if i in self.__linkBuses])
+        elif mergeHeatSourceSink and any([b in self.__heatSourceSinkBuses for b in data[from_bus].split(',')]):
+            inputBusLabel = [i + '__' + self.__buildingLabel for i in data[from_bus].split(",") if
                              i not in self.__heatSourceSinkBuses]
-            inputBusLabel.extend([i for i in data[from_bus_identifier].split(",") if i in self.__heatSourceSinkBuses])
+            inputBusLabel.extend([i for i in data[from_bus].split(",") if i in self.__heatSourceSinkBuses])
         else:
-            inputBusLabel = [i + '__' + self.__buildingLabel for i in data[from_bus_identifier].split(",")]
+            inputBusLabel = [i + '__' + self.__buildingLabel for i in data[from_bus].split(",")]
         inputBuses = [self.__busDict[i] for i in inputBusLabel]
         outputBuses = []
-        outputSHBusLabel = data[to_bus_identifier].split(",")[0] + '__' + self.__buildingLabel
-        outputDHWBusLabel = data[to_bus_identifier].split(",")[1] + '__' + self.__buildingLabel
+        outputSHBusLabel = data[to_bus].split(",")[0] + '__' + self.__buildingLabel
+        outputDHWBusLabel = data[to_bus].split(",")[1] + '__' + self.__buildingLabel
         outputBuses.append(self.__busDict[outputSHBusLabel])
         outputBuses.append(self.__busDict[outputDHWBusLabel])
         if temperatureLevels:
-            outputBusLabel3 = data[to_bus_identifier].split(",")[2] + '__' + self.__buildingLabel  # outputSHBusLabel, outputDHWBusLabel, outputBusLabel3 are in the order of increasing temperatures
+            outputBusLabel3 = data[to_bus].split(",")[2] + '__' + self.__buildingLabel  # outputSHBusLabel, outputDHWBusLabel, outputBusLabel3 are in the order of increasing temperatures
             outputBuses.append(self.__busDict[outputBusLabel3])
         envImpactPerCapacity = float(data[_ent.TransformerLabels.impact_cap]) / float(data[_ent.TransformerLabels.lifetime])
         if data[_ent.TransformerLabels.capacity_min] == 'x':
@@ -512,19 +512,19 @@ class Building:
             capacityMinSH = float(data[_ent.TransformerLabels.capacity_min])
         if pattern_at_start_followed_by_number(_ent.TransformerTypes.HP, label.prefix):
             # default coefficients for air-source heat pump
-            coefW = [0.66610, -2.2365, 15.541, 25.705, -17.407, 3.8145]
-            coefQ = [11.833, 96.504, 14.496, -50.064, 161.02, -133.60]
+            coef_W = [0.66610, -2.2365, 15.541, 25.705, -17.407, 3.8145]
+            coef_Q = [11.833, 96.504, 14.496, -50.064, 161.02, -133.60]
         elif pattern_at_start_followed_by_number(_ent.TransformerTypes.GWHP, label.prefix):
             # default coefficients for geothermal heat pump
-            coefW = [0.1600, -1.2369, 19.9391, 19.3448, 7.1057, -1.4048]
-            coefQ = [13.8978, 114.8358, -9.3634, -179.4227, 342.3363, -12.4969]
+            coef_W = [0.1600, -1.2369, 19.9391, 19.3448, 7.1057, -1.4048]
+            coef_Q = [13.8978, 114.8358, -9.3634, -179.4227, 342.3363, -12.4969]
         if (_ent.HeatPumpCoefficientLabels.coef_W in data) and (_ent.HeatPumpCoefficientLabels.coef_Q in data):
             #comma-separated entries for user-defined coefficients split into a list
-            coefW = [float(c) for c in data[_ent.HeatPumpCoefficientLabels.coef_W].split(",")]
-            coefQ = [float(c) for c in data[_ent.HeatPumpCoefficientLabels.coef_Q].split(",")]
-        heatPump = HeatPumpLinear(label.full_name, operationTempertures, temperatureAmb,
-                                  coefW,
-                                  coefQ,
+            coef_W = [float(c) for c in data[_ent.HeatPumpCoefficientLabels.coef_W].split(",")]
+            coef_Q = [float(c) for c in data[_ent.HeatPumpCoefficientLabels.coef_Q].split(",")]
+        heatPump = HeatPumpLinear(label.full_name, operationTempertures, temperature_evap,
+                                  coef_W,
+                                  coef_Q,
                                   inputBuses,
                                   outputBuses,
                                   capacityMinSH, float(data[_ent.TransformerLabels.capacity_SH]),float(data[_ent.TransformerLabels.efficiency]),
