cdbce474a4dc63eecf66b737c989cac6ad0edfd1
ahobeost <ahobeost@github.com>
2025-11-14 10:54:35 +0100

    Version according to pep 440. 0.2.2 and not 0.2.02

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def _getInstallRequirements():
 setup(
     name='optihood',
     packages=find_packages(),
-    version='v0.2.02',
+    version='v0.2.2',
     author="Institute for Solar Technology (SPF), OST Rapperswil",
     author_email="neha.dimri@ost.ch",
     description="optihood optimization framework",
